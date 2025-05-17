58293a86fbd8cf44f61f3ddb02b3db506c7ae399
nhan.trinh <hongnhan50@gmail.com>
2025-05-17 22:11:45 +0700

    Feature : add logger-service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,43 @@
+﻿package api
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "5001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	mongoClient, err := connectToMongo()
+
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func connectToMongo(*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Printf("Error connecting:", err)
+		return nil, err
+	}
+
+	return c, nil
+}
